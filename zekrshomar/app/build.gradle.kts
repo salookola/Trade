1fb396fe0117f64eec16ac7aacf1cca6cbda581f
salookola <49395241+salookola@users.noreply.github.com>
2026-07-01 07:10:14 +0330

    Bump ZekrShomar version to 1.2.0

--- zekrshomar/app/build.gradle.kts
+++ zekrshomar/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
         applicationId = "com.zekrshomar.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
     }
 }
